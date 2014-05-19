72d7e8e4bd18e9e4b72a7cd8f9a5aee0a0f022a6
David <david@david-VirtualBox.(none)>
2014-05-20 01:20:20 +0200

    update

---
+++ javascript/09_inheritance/javascript/vehicles.js
@@ -0,0 +1,67 @@
+function Car(){
+    this.wheels = 4
+	this.doors = 5
+	this.weight = 2000
+	this.payload = 400
+
+}
+
+function Motorbike(){
+    this.wheels = 4
+	this.weight = 500
+
+}
+
+function Ship(){
+    this.weight = 3000
+	this.payload = 1000
+
+}
+
+function Plane(){
+    this.doors = 2
+	this.weight = 2000
+	this.wheels = 7
+	this.payload = 2000
+
+}
+
+function Helicopter(){
+
+    this.doors = 2
+	this.payload = 1000
+	this.weight = 1000
+}
+
+function Bike(){
+    this.payload = 50
+	this.weight = 50
+	this.wheels = 2
+
+}
+
+function Skateboard(){
+    this.wheels = 8
+	this.weight = 10
+
+}
+
+function Spacecraft(){
+    this.weight = 100
+	this.payload = 200
+
+}
+
+function Tricycle(){
+    this.weight = 30
+	this.payload = 30
+	this.wheels = 3
+
+}
+
+function Donkey(){
+    this.weight = 100
+	this.payload = 100
+
+
+}
