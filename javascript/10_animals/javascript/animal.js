7c845cb8f5a303c1a0f0e7cff6c9d70d1f2c545b
David <david@david-VirtualBox.(none)>
2014-05-20 23:20:23 +0200

    problem with exercise

---
+++ javascript/10_animals/javascript/animal.js
@@ -0,0 +1,34 @@
+function animal(tipo, genero, tiempoVida){
+    this.tipo = tipo
+    this.genero = genero
+    this.tiempoVida = tiempoVida
+}
+
+vertebrado.prototype = new animal  // Extender el prototipo
+vertebrado.prototype.constructor = vertebrado // Arreglar el constructor
+
+function vertebrado(numeroHuesos){
+    this.numeroHuesos = numeroHuesos
+}
+
+invertebrado.prototype = new animal
+invertebrado.prototype.constructor = invertebrado
+
+function invertebrado(venenoso){
+    this.venenoso = venenoso 
+}
+
+perro.prototype = new vertebrado
+perro.prototype.constructor = perro
+
+function perro(){
+    vertebrado.call(this, 40)  // Llama al constructor de la superclase
+    animal.call(this, omnivoro, macho, 14)
+    this.name = "Lucas"
+}
+
+function medusa(){
+    invertebrado.call(this, "si")
+    animal.call(this, carnivoro, macho, 20)
+    this.name = "Caravela portuguesa"
+}
