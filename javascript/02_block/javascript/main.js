e3ce6496b7569a9cfe24fd4044011e094e97cf41
David <david@david-VirtualBox.(none)>
2014-05-25 17:06:21 +0200

    update

---
+++ javascript/02_block/javascript/main.js
@@ -0,0 +1,10 @@
+var a = 3
+
+function main(){
+
+    var a = 2
+	alert(a)
+
+}
+
+alert(a)
---
+++ javascript/08_menu/javascript/main.js
@@ -0,0 +1,8 @@
+function encima(element){
+    element.style.backgroundColor = "orange";
+}
+
+function fuera(element){
+    element.style.backgroundColor="yellow";
+
+}
